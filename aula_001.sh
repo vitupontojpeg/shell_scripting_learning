eeb425add8b6309c28dc1e52f723eadb81931472
Vitor de Paula Santos <vituvitosogames@gmail.com>
2026-06-16 20:36:18 -0300

    restart the machine kkk

---
+++ aula_001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#------------------------
+#    Author: Vitor de Paula Santos
+#    Git: 
+#    CreateDate: 16/06/2026
+#    UpdateDate: 16/06/2026
+#------------------------
+
+FRUITLIST=(orange strawberry lemon pinneaple)
+echo ${FRUITLIST[*]}
+
+# update a item
+FRUITLIST[1]=banana
+echo ${FRUITLIST[*]}
+
+# deleting a item
+unset FRUITLIST[1]
+echo ${FRUITLIST[*]}
+
+# adding a item
+
+FRUITLIST[4]=orange
+echo ${FRUITLIST[*]}
