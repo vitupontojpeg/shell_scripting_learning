39415a5cd576c4da2a68e23330fc86420f62a077
Vitor de Paula Santos <vituvitosogames@gmail.com>
2026-06-29 18:55:05 -0300

    echo command

---
+++ echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#------------------------
+#    Author: Vitor de Paula Santos
+#    Git: 
+#    CreateDate: 29/06/2026
+#    UpdateDate: 29/06/2026
+#------------------------
+
+cd /
+
+ls -l
+
+# esta foi a minha listagem de diretorios da raiz 
