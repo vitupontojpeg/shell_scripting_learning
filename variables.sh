0d683d22a11b983f0404ee3305ab5a59344ab306
Vitor de Paula Santos <vituvitosogames@gmail.com>
2026-06-30 20:23:06 -0300

    working with variables

---
+++ variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#------------------------
+#    Author: Vitor de Paula Santos
+#    Git: 
+#    CreatedDate: 30/06/2026
+#    UpdatedDate: 30/06/2026
+#------------------------
+
+echo "User: $USER"
+echo "home dic: $HOME"
+echo "user UID:$UID"
+
+age=25
+name="Vitor"
+echo "The $name is $age years old"
